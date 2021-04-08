770d0e2679f29a6dda29469c7aa4b6f3f98d12b3
Abdelhakim RASFI <7268784-Abdelhakim.rasfi@users.noreply.gitlab.com>
2021-04-08 04:18:14 +0200

    Added: deleteItem

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { View, Text, StyleSheet, FlatList} from 'react-native';
 import Header from './components/Header';
+import ListItem from './components/ListItem';
 // import { uuid } from 'uuidv4';
 const App = () => {
 
@@ -11,13 +12,19 @@ const App = () => {
         {id: 4, text: 'Juice'},
     ])
 
+    const deleteItem = (id) => {
+        setItems(prevItems => {
+            return prevItems.filter(item => item.id != id)
+        });
+    }
+
     return ( 
         <View style={styles.container}>
             <Header title="Shopping Cart" />
             <FlatList
              data = {items}
              renderItem = {({item}) => 
-                <Text>{item.text}</Text>
+                <ListItem item = {item} deleteItem = {deleteItem} />
              } />
         </View>
     );
