6b3566bed4f0bda9848aa48e2841e8de7954542f
Abdelhakim RASFI <7268784-Abdelhakim.rasfi@users.noreply.gitlab.com>
2021-04-08 04:40:21 +0200

    Added: Additem

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet, FlatList} from 'react-native';
+import { View, Text, StyleSheet, FlatList, Alert} from 'react-native';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 // import { uuid } from 'uuidv4';
 const App = () => {
 
@@ -18,9 +19,23 @@ const App = () => {
         });
     }
 
+    const addItem = (text) => {
+        if (!text) 
+        {
+            Alert.alert('Error', 'Please enter an item.')
+        }
+        else
+        {
+            setItems(prevItems => {
+                return [{id: 5, text: text} , ...prevItems]
+            })
+        }
+    }
+
     return ( 
         <View style={styles.container}>
-            <Header title="Shopping Cart" />
+            <Header title="Shopping List" />
+            <AddItem addItem={addItem} />
             <FlatList
              data = {items}
              renderItem = {({item}) => 
