798d37c0ce290518d77b72e9443081eddc53e289
Abdelhakim RASFI <7268784-Abdelhakim.rasfi@users.noreply.gitlab.com>
2021-04-08 03:16:12 +0200

    Add StyleSheet

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { View, Text} from 'react-native'
+import { View, Text, StyleSheet} from 'react-native'
 
 const App = () => {
     return ( 
-        <View style={styles.container} >
+        <View style={styles.container}>
             <Text style={styles.text} >Hello World</Text>
         </View>
     );
